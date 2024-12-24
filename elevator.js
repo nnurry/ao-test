8221b1f7f798522df9f3d5100e6b3bb9603cd51a
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2024-12-25 01:55:53 +0700

    WIP: Implement basic scheduler

--- elevator.js
+++ elevator.js
@@ -17,93 +17,38 @@ class Elevator {
         this.moveTime = moveTime;
     }
 
-    cost(upFirst = true, ...segments) {
-        let currentFloor = this.curr;
-
-        let moveTime = 0;
-        let waitTime = 0;
-
-        const order = upFirst ? [0, 1, 2] : [2, 1, 0];
-        for (const idx of order) {
-            const segment = segments[idx];
-            if (segment && segment.length > 0) {
-                const targetFloor = segment[segment.length - 1].floor;
-                const segmentMoveTime = Math.abs(targetFloor - currentFloor) * this.moveTime;
-                const segmentWaitTime = segment.length * this.waitTime;
-                moveTime += segmentMoveTime;
-                waitTime += segmentWaitTime;
-                currentFloor = targetFloor;
-            }
-        }
-        return moveTime + waitTime;
-    }
-
-    call(floor, dir) {
-        function up(ups, downs) {
-            // sort the up requests
-            // move requests of lower floors to the end of the list
-            let endOfSmallers = 0;
-            for (const i = 0; i < ups.length; i++) {
-                if (ups[i] >= this.curr) {
-                    break;
-                }
-                endOfSmallers = i;
-            }
-            if (endOfSmallers > 0) {
-                return [ups.slice(endOfSmallers), downs, ups.slice(0, endOfSmallers)];
-            }
-            return [ups, downs, []];
-        }
-        function down(ups, downs) {
-            // sort the down requests
-            // move requests of higher floors to the end of the list
-            let endOfBiggers = 0;
-            for (const i = 0; i < downs.length; i++) {
-                if (downs[i] <= this.curr) {
-                    break;
-                }
-                endOfBiggers = i;
-            }
-            if (endOfBiggers > 0) {
-                return [downs.slice(endOfBiggers), ups, downs.slice(0, endOfBiggers)];
-            }
-            return [downs, ups, []];
-        }
-        const isDuplicated = this.requests.some((req) =>
-            req.floor === floor
-            && req.direction === dir
-        );
-
-        if (!isDuplicated) {
-            this.requests.push({ floor, dir });
-
-            let upRequests = this.requests.filter(req => req.dir === Direction.UP).sort((a, b) => a.floor < b.floor);
-            let downRequests = this.requests.filter(req => req.dir === Direction.DOWN).sort((a, b) => a.floor > b.floor);
-
-            if (this.dir === Direction.UP) {
-                // sort the up requests
-                // move requests of lower floors to the end of the list
-                const segmentedMoves = this.up(upRequests, downRequests);
-                this.requests = segmentedMoves.flat();
-            }
-            else if (this.dir === Direction.DOWN) {
-                // sort the down requests
-                // move requests of higher floors to the end of the list
-                const segmentedMoves = this.down(upRequests, downRequests);
-                this.requests = segmentedMoves.flat();
-            }
-            else {
-                // find optimal trip (down first or up first)
-                const segmentedUpMoves = this.up(upRequests, downRequests);
-                const segmentedDownMoves = this.down(upRequests, downRequests);
-                let chooseUp = this.cost(true, ...segmentedUpMoves) < this.cost(false, ...segmentedDownMoves);
-                this.requests = chooseUp ? segmentedUpMoves.flat() : segmentedDownMoves.flat();
-            }
-        }
-    }
-
     move() {
-        // TODO
+        if (this.requests.length === 0) {
+            this.dir = Direction.IDLE;
+            return;
+        }
+
+        const nextDest = this.requests[0];
+
+        if (this.curr < nextDest.floor) {
+            this.dir = Direction.UP;
+            this.curr++;
+        }
+
+        else if (this.curr > nextDest.floor) {
+            this.dir = Direction.DOWN;
+            this.curr--;
+        }
+
+        if (this.curr === nextDest.floor) {
+            this.open();
+            // Remove all requests for the current floor (important for same-floor opposite-direction requests)
+            this.requests = this.requests.filter(req => req.floor !== this.curr);
+
+            clearTimeout()
+            setTimeout(() => {
+                this.close();
+                // After closing the door, check if there are more requests, if none, set direction to idle
+                if (this.requests.length === 0) {
+                    this.dir = Direction.IDLE;
+                }
+            }, this.waitTime);
+        }
     }
 
     open() {
@@ -115,4 +60,101 @@ class Elevator {
     }
 }
 
-module.exports = Elevator;
+class Scheduler {
+    constructor() {
+        // there might be some internal states
+    }
+
+    costFunction(elevator, upFirst = true, ...segments) {
+        let currentFloor = elevator.curr;
+
+        let moveTime = 0;
+        let waitTime = 0;
+
+        const order = upFirst ? [0, 1, 2] : [2, 1, 0];
+        for (const idx of order) {
+            const segment = segments[idx];
+            if (segment && segment.length > 0) {
+                const targetFloor = segment[segment.length - 1].floor;
+                const segmentMoveTime = Math.abs(targetFloor - currentFloor) * elevator.moveTime;
+                const segmentWaitTime = segment.length * elevator.waitTime;
+                moveTime += segmentMoveTime;
+                waitTime += segmentWaitTime;
+                currentFloor = targetFloor;
+            }
+        }
+        return moveTime + waitTime;
+    }
+
+    scheduleMoveUpFirst(elevator, ups, downs) {
+        // sort the up requests
+        // move requests of lower floors to the end of the list
+        let endOfSmallers = 0;
+        for (let i = 0; i < ups.length; i++) {
+            if (ups[i] >= elevator.curr) {
+                break;
+            }
+            endOfSmallers = i;
+        }
+        if (endOfSmallers > 0) {
+            return [ups.slice(endOfSmallers), downs, ups.slice(0, endOfSmallers)];
+        }
+        return [ups, downs, []];
+    }
+    scheduleMoveDownFirst(elevator, ups, downs) {
+        // sort the down requests
+        // move requests of higher floors to the end of the list
+        let endOfBiggers = 0;
+        for (let i = 0; i < downs.length; i++) {
+            if (downs[i] <= elevator.curr) {
+                break;
+            }
+            endOfBiggers = i;
+        }
+        if (endOfBiggers > 0) {
+            return [downs.slice(endOfBiggers), ups, downs.slice(0, endOfBiggers)];
+        }
+        return [downs, ups, []];
+    }
+
+    schedule(elevator) {
+        const upRequests = elevator.requests.filter(req => req.dir === Direction.UP).sort((a, b) => a.floor < b.floor);
+        const downRequests = elevator.requests.filter(req => req.dir === Direction.DOWN).sort((a, b) => a.floor > b.floor);
+
+        if (elevator.dir === Direction.UP) {
+            // sort the up requests
+            // move requests of lower floors to the end of the list
+            const segmentedMoves = this.scheduleMoveUpFirst(elevator, upRequests, downRequests);
+            return segmentedMoves.flat();
+        }
+        else if (elevator.dir === Direction.DOWN) {
+            // sort the down requests
+            // move requests of higher floors to the end of the list
+            const segmentedMoves = this.scheduleMoveDownFirst(elevator, upRequests, downRequests);
+            return segmentedMoves.flat();
+        }
+        else {
+            // find optimal trip (down first or up first)
+            const segmentedUpMoves = this.scheduleMoveUpFirst(elevator, upRequests, downRequests);
+            const segmentedDownMoves = this.scheduleMoveDownFirst(elevator, upRequests, downRequests);
+            const costOfUpFirst = this.costFunction(elevator, true, ...segmentedUpMoves);
+            const costOfDownFirst = this.costFunction(elevator, false, ...segmentedDownMoves);
+            const chooseUp = costOfUpFirst > costOfDownFirst;
+            return chooseUp ? segmentedUpMoves.flat() : segmentedDownMoves.flat();
+        }
+    }
+
+    call(elevator, floor, dir) {
+        const isDuplicated = elevator.requests.some((req) =>
+            req.floor === floor
+            && req.direction === dir
+        );
+
+        if (!isDuplicated) {
+            elevator.requests.push({ floor, dir });
+            elevator.schedule = this.schedule(elevator);
+        }
+    }
+}
+
+module.exports = { Elevator, Scheduler, Direction };
