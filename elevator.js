0e6f4a720c8b4b44c17111a44d9336aafdfd5d0c
Nguyen Nhat Anh <nhatanhcd2169@yahoo.com>
2024-12-25 09:44:39 +0700

    WIP: Add sum loggings

--- elevator.js
+++ elevator.js
@@ -20,32 +20,38 @@ class Elevator {
     move() {
         if (this.requests.length === 0) {
             this.dir = Direction.IDLE;
+            console.log(`Elevator ${this.id}: No requests. Setting direction to IDLE.`);
             return;
         }
 
         const nextDest = this.requests[0];
+        console.log(`Elevator ${this.id}: Current floor: ${this.curr}. Next destination: ${nextDest.floor}. Direction: ${this.dir}`);
 
         if (this.curr < nextDest.floor) {
             this.dir = Direction.UP;
             this.curr++;
-        }
-
-        else if (this.curr > nextDest.floor) {
+            console.log(`Elevator ${this.id}: Moving UP to floor ${this.curr}.`);
+        } else if (this.curr > nextDest.floor) {
             this.dir = Direction.DOWN;
             this.curr--;
+            console.log(`Elevator ${this.id}: Moving DOWN to floor ${this.curr}.`);
         }
 
         if (this.curr === nextDest.floor) {
+            console.log(`Elevator ${this.id}: Reached floor ${this.curr}. Opening doors.`);
             this.open();
             // Remove all requests for the current floor (important for same-floor opposite-direction requests)
             this.requests = this.requests.filter(req => req.floor !== this.curr);
 
-            clearTimeout()
             setTimeout(() => {
+                console.log(`Elevator ${this.id}: Doors closing.`);
                 this.close();
                 // After closing the door, check if there are more requests, if none, set direction to idle
                 if (this.requests.length === 0) {
                     this.dir = Direction.IDLE;
+                    console.log(`Elevator ${this.id}: No remaining requests. Setting direction to IDLE.`);
+                } else {
+                    console.log(`Elevator ${this.id}: Remaining requests: ${JSON.stringify(this.requests)}`);
                 }
             }, this.waitTime);
         }
@@ -53,108 +59,13 @@ class Elevator {
 
     open() {
         this.isOpen = true;
+        console.log(`Elevator ${this.id}: Doors opened.`);
     }
 
     close() {
         this.isOpen = false;
+        console.log(`Elevator ${this.id}: Doors closed.`);
     }
 }
 
-class Scheduler {
-    constructor() {
-        // there might be some internal states
-    }
-
-    costFunction(elevator, upFirst = true, ...segments) {
-        let currentFloor = elevator.curr;
-
-        let moveTime = 0;
-        let waitTime = 0;
-
-        const order = upFirst ? [0, 1, 2] : [2, 1, 0];
-        for (const idx of order) {
-            const segment = segments[idx];
-            if (segment && segment.length > 0) {
-                const targetFloor = segment[segment.length - 1].floor;
-                const segmentMoveTime = Math.abs(targetFloor - currentFloor) * elevator.moveTime;
-                const segmentWaitTime = segment.length * elevator.waitTime;
-                moveTime += segmentMoveTime;
-                waitTime += segmentWaitTime;
-                currentFloor = targetFloor;
-            }
-        }
-        return moveTime + waitTime;
-    }
-
-    scheduleMoveUpFirst(elevator, ups, downs) {
-        // sort the up requests
-        // move requests of lower floors to the end of the list
-        let endOfSmallers = 0;
-        for (let i = 0; i < ups.length; i++) {
-            if (ups[i] >= elevator.curr) {
-                break;
-            }
-            endOfSmallers = i;
-        }
-        if (endOfSmallers > 0) {
-            return [ups.slice(endOfSmallers), downs, ups.slice(0, endOfSmallers)];
-        }
-        return [ups, downs, []];
-    }
-    scheduleMoveDownFirst(elevator, ups, downs) {
-        // sort the down requests
-        // move requests of higher floors to the end of the list
-        let endOfBiggers = 0;
-        for (let i = 0; i < downs.length; i++) {
-            if (downs[i] <= elevator.curr) {
-                break;
-            }
-            endOfBiggers = i;
-        }
-        if (endOfBiggers > 0) {
-            return [downs.slice(endOfBiggers), ups, downs.slice(0, endOfBiggers)];
-        }
-        return [downs, ups, []];
-    }
-
-    schedule(elevator) {
-        const upRequests = elevator.requests.filter(req => req.dir === Direction.UP).sort((a, b) => a.floor < b.floor);
-        const downRequests = elevator.requests.filter(req => req.dir === Direction.DOWN).sort((a, b) => a.floor > b.floor);
-
-        if (elevator.dir === Direction.UP) {
-            // sort the up requests
-            // move requests of lower floors to the end of the list
-            const segmentedMoves = this.scheduleMoveUpFirst(elevator, upRequests, downRequests);
-            return segmentedMoves.flat();
-        }
-        else if (elevator.dir === Direction.DOWN) {
-            // sort the down requests
-            // move requests of higher floors to the end of the list
-            const segmentedMoves = this.scheduleMoveDownFirst(elevator, upRequests, downRequests);
-            return segmentedMoves.flat();
-        }
-        else {
-            // find optimal trip (down first or up first)
-            const segmentedUpMoves = this.scheduleMoveUpFirst(elevator, upRequests, downRequests);
-            const segmentedDownMoves = this.scheduleMoveDownFirst(elevator, upRequests, downRequests);
-            const costOfUpFirst = this.costFunction(elevator, true, ...segmentedUpMoves);
-            const costOfDownFirst = this.costFunction(elevator, false, ...segmentedDownMoves);
-            const chooseUp = costOfUpFirst > costOfDownFirst;
-            return chooseUp ? segmentedUpMoves.flat() : segmentedDownMoves.flat();
-        }
-    }
-
-    call(elevator, floor, dir) {
-        const isDuplicated = elevator.requests.some((req) =>
-            req.floor === floor
-            && req.direction === dir
-        );
-
-        if (!isDuplicated) {
-            elevator.requests.push({ floor, dir });
-            elevator.schedule = this.schedule(elevator);
-        }
-    }
-}
-
-module.exports = { Elevator, Scheduler, Direction };
+module.exports = { Elevator, Direction };
--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const { Elevator, Scheduler, Direction } = require("./elevator.js");
+const { Elevator, Direction } = require("./elevator.js");
+const { Scheduler } = require("./scheduler.js");
 
 // Example usage demonstrating the scenarios:
 const elevator = new Elevator(1, 10);
 const scheduler = new Scheduler();
 
+console.log("Starting Elevator Simulation\n");
+
 // Scenario 1: Single Upward Request
 console.log("Scenario 1: Single Upward Request");
 scheduler.call(elevator, 2, Direction.UP);
-setInterval(() => elevator.move(), elevator.moveTime);
+console.log("Request added: Floor 2, Direction UP");
+
+let interval = setInterval(() => {
+    console.log(`Elevator moving. Current floor: ${elevator.curr}, Direction: ${elevator.dir}`);
+    elevator.move();
+}, elevator.moveTime);
 
 setTimeout(() => {
+    console.log("\nEnding Scenario 1");
     clearInterval(interval);
     elevator.requests = [];
     elevator.curr = 1;
@@ -18,13 +27,18 @@ setTimeout(() => {
     // Scenario 2: Multiple Upward Requests
     console.log("\nScenario 2: Multiple Upward Requests");
     scheduler.call(elevator, 3, Direction.UP);
+    console.log("Request added: Floor 3, Direction UP");
     scheduler.call(elevator, 4, Direction.UP);
-    interval = setInterval(() => elevator.move(), elevator.moveTime);
-}, 5000)
+    console.log("Request added: Floor 4, Direction UP");
 
-let interval = null;
+    interval = setInterval(() => {
+        console.log(`Elevator moving. Current floor: ${elevator.curr}, Direction: ${elevator.dir}`);
+        elevator.move();
+    }, elevator.moveTime);
+}, 5000);
 
 setTimeout(() => {
+    console.log("\nEnding Scenario 2");
     clearInterval(interval);
     elevator.requests = [];
     elevator.curr = 3;
@@ -33,11 +47,18 @@ setTimeout(() => {
     // Scenario 3: Mixed Requests
     console.log("\nScenario 3: Mixed Requests");
     scheduler.call(elevator, 5, Direction.UP);
+    console.log("Request added: Floor 5, Direction UP");
     scheduler.call(elevator, 1, Direction.DOWN);
-    interval = setInterval(() => elevator.move(), elevator.moveTime);
-}, 10000)
+    console.log("Request added: Floor 1, Direction DOWN");
+
+    interval = setInterval(() => {
+        console.log(`Elevator moving. Current floor: ${elevator.curr}, Direction: ${elevator.dir}`);
+        elevator.move();
+    }, elevator.moveTime);
+}, 10000);
 
 setTimeout(() => {
+    console.log("\nEnding Scenario 3");
     clearInterval(interval);
     elevator.requests = [];
     elevator.curr = 2;
@@ -46,6 +67,18 @@ setTimeout(() => {
     // Scenario 4: Same Floor, Opposite Direction Requests
     console.log("\nScenario 4: Same Floor, Opposite Direction Requests");
     scheduler.call(elevator, 2, Direction.UP);
+    console.log("Request added: Floor 2, Direction UP");
     scheduler.call(elevator, 2, Direction.DOWN);
-    interval = setInterval(() => elevator.move(), elevator.moveTime);
-}, 15000)
+    console.log("Request added: Floor 2, Direction DOWN");
+
+    interval = setInterval(() => {
+        console.log(`Elevator moving. Current floor: ${elevator.curr}, Direction: ${elevator.dir}`);
+        elevator.move();
+    }, elevator.moveTime);
+}, 15000);
+
+setTimeout(() => {
+    console.log("\nEnding Scenario 4");
+    clearInterval(interval);
+    console.log("Elevator simulation completed.");
+}, 20000);
